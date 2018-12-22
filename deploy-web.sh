3696781afa874ffb2de805a938e90b7ff7eada71
mundi <hello@mandychoi.com.au>
2018-12-22 11:40:18 +0000

    #1 delete all - refresh start

--- deploy-web.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sass web/scss/:web/styles/ --style compressed
-
-export S3_DIRECTORY=web
-export S3_BUCKET_NAME=lmc-web-portfolio
-
-aws s3 sync $S3_DIRECTORY s3://$S3_BUCKET_NAME/ --acl=public-read
